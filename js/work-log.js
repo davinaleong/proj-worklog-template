0ca6e069b1dd2f1461d35bd2ed4c1dde5a199696
Personal Profile <davina.leong.sy@gmail.com>
2019-05-29 19:52:47 +0800

    Added link to edit form

--- js/work-log.js
+++ js/work-log.js
@@ -3,28 +3,71 @@
  */
 
 // Declare element properties
-const workLogClickable = {
+const worklogCard = {
   selector: '.clickable.worklog-card',
-  jqElement: null
+  jqElement: null,
+  location: 'existing-log.html'
 }
+
+const worklogCardEdit = {
+  selector: '.clickable.worklog-card-edit',
+  jqElement: null,
+  location: 'existing-log-edit.html'
+}
+
+const jqObjects = [];
 
 $(document).ready(function () {
 
-  // Assign jQuery elements by their selectors
-  workLogClickable.jqElement = $(workLogClickable.selector)
+  addJqObjectsToArray()
 
-  registerEvents()
+  initJqElements()
+  registerJqEvents()
 
 })
 
 /**
+ * Add objects to jqObjects array
+ */
+function addJqObjectsToArray() {
+
+  console.log('--- fn addJqObjectsToArray')
+
+  jqObjects.push(worklogCard)
+  jqObjects.push(worklogCardEdit)
+
+}
+
+/**
+ * Initialise jQuery elements
+ */
+function initJqElements() {
+
+  console.log('--- fn initJqElements')
+
+  // worklogCard.jqElement = $(worklogCard.selector)
+  // worklogCardEdit.jqElement = $(worklogCardEdit.selector)
+  jqObjects.map(jqObject => {
+    jqObject.jqElement = $(jqObject.selector)
+  })
+
+}
+
+/**
  * Register events on jQuery elements
  */
-function registerEvents() {
+function registerJqEvents() {
+
+  console.log('--- fn registerJqEvents')
 
   // Go to 'existing-log' html page when a work-log card is clicked.
-  workLogClickable.jqElement.click(function() {
-    window.location.href = './existing-log.html'
+  worklogCard.jqElement.click(function() {
+    window.location.href = worklogCard.location
+  })
+
+  // Go to 'existing-log-edit' html page when a work-log card is clicked.
+  worklogCardEdit.jqElement.click(function() {
+    window.location.href = worklogCardEdit.location
   })
 
 }
